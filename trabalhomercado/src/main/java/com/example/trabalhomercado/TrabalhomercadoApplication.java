2cada5ab19f83f7d80b2fca3b408b25a10b2338e
DanLessa-png <danlessa9@gmail.com>
2025-12-01 11:19:17 -0300

    Backend

--- trabalhomercado/src/main/java/com/example/trabalhomercado/TrabalhomercadoApplication.java
+++ trabalhomercado/src/main/java/com/example/trabalhomercado/TrabalhomercadoApplication.java
@@ -17,9 +17,13 @@ public class TrabalhomercadoApplication {
     @Bean
     public CommandLineRunner initData(ProdutoRepository produtoRepository) {
         return args -> {
-            produtoRepository.save(new Produto("Maçã", 2.50, "Maçã vermelha fresca", "Frutas"));
-            produtoRepository.save(new Produto("Banana", 1.80, "Banana prata", "Frutas"));
-            produtoRepository.save(new Produto("Arroz", 5.00, "Arroz branco 1kg", "Grãos"));
+            produtoRepository.save(new Produto("Arroz", 5.89, "Arroz branco", "Grãos"));
+            produtoRepository.save(new Produto("Feijão", 7.43, "Feijão preto", "Grãos"));
+            produtoRepository.save(new Produto("Farinha", 3.34, "Farinha branca", "Grãos"));
+            produtoRepository.save(new Produto("Café", 10.45, "Café gão", "Grãos"));
+            produtoRepository.save(new Produto("Macarrão", 7.34, "Macarrão", "Industrializados"));
+            produtoRepository.save(new Produto("Carne", 25.65, "Carne de porco", "Carnes"));
+
         };
     }
 }
